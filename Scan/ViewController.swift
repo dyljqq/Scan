6d230baf97d69a57169a231b4ba14f0aa1d72239
jiqinqiang <355086587@qq.com>
2021-07-17 02:32:47 +0800

    Initial Commit

---
+++ Scan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scan
+//
+//  Created by 季勤强 on 2021/7/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
